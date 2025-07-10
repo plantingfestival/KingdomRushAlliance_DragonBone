c4edf384e7a29c59d77cb65d0148ef71fbf2efec
plantingfestival <plantingfestival@126.com>
2025-07-10 19:57:45 +0800

    Revert "适配懒人包"
    This reverts commit 6f3d60c5aa017923fe891832604695409b604cee.

--- _assets/all-desktop/shaders/p_add.c
+++
@@ -1,9 +0,0 @@
-// 
-// adds the color to the texture
-//
-
-vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
-{
-    vec4 texcolor = Texel(tex, texture_coords);
-    return texcolor + color;
-}
