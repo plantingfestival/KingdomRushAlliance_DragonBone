c67fe5ea25467adb463194c8290c992451c21aa9
plantingfestival <plantingfestival@126.com>
2025-09-24 11:51:34 +0800

    修改英文文本的问题

--- _assets/kr5-desktop/strings/en.lua
+++ _assets/kr5-desktop/strings/en.lua
@@ -3235,7 +3235,7 @@ return {
 	LEVEL_417_TITLE = "417. MAGINICIA SHORES",
 	LEVEL_418_HISTORY = "Look, General! See how they flee as our mighty army approaches with relentless strength. We only need to defeat their garrison and this place will be ours! But do beware, I sense something… odd in the magic emanating from this place. Could this be the power of darkness I'm sensing? If I'm correct, they must be stopped without a moment to waste.",
 	LEVEL_418_TITLE = "418. ANURIAN PLAZA",
-	LEVEL_419_HISTORY = "What I feared is true… Polyx, the sage, delved too deeply into forbidden mysteries and now suffers the tainting grasp of corruption. Did he really think he could control the power of darkness and walk away unscathed? Defeat him and his followers before he's able to influence this land. Failure is not an option!"
+	LEVEL_419_HISTORY = "What I feared is true… Polyx, the sage, delved too deeply into forbidden mysteries and now suffers the tainting grasp of corruption. Did he really think he could control the power of darkness and walk away unscathed? Defeat him and his followers before he's able to influence this land. Failure is not an option!",
 	LEVEL_419_TITLE = "419. POND OF THE SAGE",
 	LEVEL_426_HISTORY = "I knew that this moment would come. The greatest ghosts of Valardul want me to pay off my debts with them. But their claims are too high! I will not give them half of the living beings of my realms! They are coming with their most elite creatures. Fortunately, I took my precautions.",
 	LEVEL_426_TITLE = "426. BACK TO THE ROTTEN FOREST",
